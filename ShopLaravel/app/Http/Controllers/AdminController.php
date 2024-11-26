11126cbd1e97adaf8ba797ca7a75d3c5bd98bfbd
Tú Quyên <nttquyen1512@gmail.com>
2024-11-26 22:13:03 +0700

    update

---
+++ ShopLaravel/app/Http/Controllers/AdminController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
+
+session_start();
+
+class AdminController extends Controller
+{
+    //
+    public function index() {
+        return view('admin_login');
+    }
+
+    public function show_dashboard() {
+        return view('admin_layout');
+    }
+
+    public function showdashboard() {
+        $this->AuthLogin();
+        return view('admin.dashboard');
+    }
+
+    public function dashboard(Request $request){
+        $admin_email = $request->admin_email;
+        $admin_password = md5($request->admin_password);
+        $result = DB::table('tbl_admin')->where('admin_email',$admin_email)->where('admin_password',$admin_password)->first();
+        if ($result) {
+            Session::put('admin_name', $result->admin_name);
+            Session::put('admin_id', $result->admin_id);
+            return view('admin.dashboard');
+        }
+         else{
+            Session::put('message','mat khau hoac email khong dung, nhap lai nhe');
+            return Redirect::to('/admin');
+        }
+    }
+
+    public function logout(){
+        Session::put('admin_name',null);
+            Session::put('admin_id',null);
+            return Redirect::to('/admin');
+
+    }
+
+    public function AuthLogin(){
+        $admin_id = Session::get('admin_id');
+        if($admin_id){
+            return Redirect::to('dashboard');
+        }else{
+            return Redirect::to('admin')->send();
+        }
+    }
+}
---
+++ ShopLaravel/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/trang-chu', [HomeController::class, 'index']);
+Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/dashboard', [AdminController::class, 'show_dashboard']);
+Route::post('/admin-dashboard', [AdminController::class, 'show_dashboard']);
+Route::get('/logout', [AdminController::class, 'logout']);
+
+
+
+
+
